d551413204f7f04ca49ab85e210fe25f48150735
Aidarov Kirill <github@aidarov.ru>
2023-03-19 12:38:52 +0300

    update main.py removed the duplicate rules description

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def get_next_open_row(board, col):
             return r
 
 '''
+# промежуточная реализация печати доски используя np.flip
 def print_board(board):
     print(np.flip(board, 0))
 '''
@@ -110,48 +111,3 @@ while not game_over:
 
     turn += 1
     turn = turn % 2
-
-
-
-
-'''
-Connect Four is a 2-player game. Players
-alternate taking turns. Each side of the
-Connect Four board has its own color of
-checkers. There is one color for each player.
-The goal of Connect Four is to get 4 of your
-color checkers in a row—horizontally,
-vertically, or diagonally—before your
-opponent does!
-When it is your turn, drop 1 of your checkers
-into the open slot at the top of the Connect
-Four board. This allows you to either build
-your row or stop your opponent from
-getting 4 in a row. After you drop your
-checker, it’s your opponent’s turn.
-The game is over when one player gets 4
-checkers in a row, or when all slots are filled,
-meaning the game ends in a stalemate.
-To play again, pull the slides out of the
-bottom of the board, releasing the checkers
-onto the ground. There is one slide on each
-end of the board.
-Happy Connect Four-ing!
-'''
-
-'''
-Connect Four - это игра для двух игроков. Игроки чередуются по очереди. 
-Каждая сторона доски Connect Four имеет свой цвет шашки. 
-Для каждого игрока существует один цвет.
-Цель игры "Соедини четыре" - собрать 4 шашки своего цвета шашек в ряд 
-- по горизонтали, по вертикали или диагонали - до того, как это сделает ваш до того, 
-как это сделает ваш противник!
-Когда наступит ваша очередь, опустите 1 из ваших шашек в открытый слот в верхней части доски Connect 4. 
-Это позволит вам либо построить свой ряд или помешать противнику получить 4 шашки в ряд. 
-После того, как вы бросите свою шашку, наступает очередь вашего противника.
-Игра заканчивается, когда один из игроков получает 4 шашек подряд или когда все слоты заполнены, 
-то есть игра заканчивается в тупике.
-Чтобы сыграть снова, вытащите полозья из из нижней части доски, освободив шашки на землю. 
-На каждом конце доски.
-Счастливой игры!
-'''
